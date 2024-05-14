3765c1f145667fdd1d083a248c9ab56c59bce24c
Anna Maria <aannamariabr@outlook.com>
2024-05-14 16:19:41 -0300

    Tasks CRUD

--- app.py
+++ app.py
@@ -1,10 +1,80 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task(): 
+  global task_id_control
+  data = request.get_json()
+
+  new_task = Task(
+    id=task_id_control, 
+    title=data['title'], 
+    description=data['description']
+  )
+  task_id_control+=1
+  tasks.append(new_task)
+  print(tasks)
+
+  return jsonify({"message": "New task created!"})
+
+@app.route('/tasks', methods=['GET'])
+def get_taks():
+  task_list = [task.to_dict() for task in tasks]
+
+  output = {
+    "tasks": task_list,
+    "total_tasks": len(task_list)
+  }
+  
+  return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+  for t in tasks:
+    if t.id == id:
+      return jsonify(t.to_dict())
+    
+  return jsonify({"message": "Task not found"}), 404
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+  task = None 
+  
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+
+  if task == None:
+    return jsonify({"message": "Task not found"}), 404
+  
+  data = request.get_json()
+
+  task.title = data['title']
+  task.description = data['description']
+  task.completed = data['completed']
+
+  return jsonify({"message": "Task updated successfully"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+  task = None
+  
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+
+  if task == None:
+    return jsonify({"message": "Task not found"}), 404
+  
+  tasks.remove(task)
+  return jsonify({"message": "Task deleted successfully"})
 
 if __name__ == "__main__":
   app.run(debug=True)
